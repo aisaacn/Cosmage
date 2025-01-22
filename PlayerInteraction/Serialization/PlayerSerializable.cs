4cb655d26964f0bfdcfabeca4c608d939f9cec10
Isaac Austin <aisaacn@gmail.com>
2025-01-21 23:33:29 -0600

    Converted project to use .NET 9.0. Added preliminary JsonConverter for Item to allow for Player serialization

---
+++ PlayerInteraction/Serialization/PlayerSerializable.cs
@@ -0,0 +1,16 @@
+﻿using CosmageV2.PlayerInteraction.Itemization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmageV2.PlayerInteraction.Serialization
+{
+    public class PlayerSerializable
+    {
+        public string Name { get; set; }
+        public Element Element { get; set; }
+        public List<Item> Items { get; set; } = [];
+    }
+}
